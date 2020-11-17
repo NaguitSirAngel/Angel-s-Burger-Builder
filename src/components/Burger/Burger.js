95148e387c5151417390244bbb7567324c28a49c
Sir Angel Naguit <angelnaguit@icloud.com>
2020-11-17 00:16:17 -0500

    Dynamic Burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,21 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 
 const burger = (props) => {
+    //convert props.ingredient obj to array
+    //.keys() returns arrays 
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => { //console.log('hey ' + igKey);
+        //console.log(props.ingredients[igKey]);
+        //console.log(Array(props.ingredients[igKey]));
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key ={igKey + i} type={igKey}/>
+        }) // [ whatever , 1]
+    });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
