df9d905798c286234288e327a371efff43641f1b
Sir Angel Naguit <angelnaguit@icloud.com>
2020-11-17 18:32:14 -0500

    BuilderControls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,15 +6,22 @@ import classes from './Burger.module.css';
 const burger = (props) => {
     //convert props.ingredient obj to array
     //.keys() returns arrays 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => { //console.log('hey ' + igKey);
         //console.log(props.ingredients[igKey]);
         //console.log(Array(props.ingredients[igKey]));
         return [...Array(props.ingredients[igKey])].map((_, i) => {
+            //console.log(igKey + i)
             return <BurgerIngredient key ={igKey + i} type={igKey}/>
         }) // [ whatever , 1]
-    });
-
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    //console.log(transformedIngredients);
+    //checking if no ingredients
+    if(transformedIngredients.length === 0) {
+        transformedIngredients= <p>Please add ingredients.</p>
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
