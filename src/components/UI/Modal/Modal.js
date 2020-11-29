392fc440b0eeac686da357b4c372576cdad28971
Sir Angel Naguit <angelnaguit@icloud.com>
2020-11-29 00:20:22 -0500

    Added Backdrop
    Backdrop styled
    Cancel order when backdrop is clicked

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
+import Aux from '../../../hoc/Auxillary';
+import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 const modal = (props) => (
-    <div
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
         style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
                 opacity: props.show ? '1':'0'}} 
         className={classes.Modal}>
         {props.children}
-    </div>
+    </div> 
+    </Aux>
 );
 
 export default modal
