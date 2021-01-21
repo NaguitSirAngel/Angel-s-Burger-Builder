febef53ebc68124675d397cefb1185b4cf52ea67
Sir Angel Naguit <angelnaguit@icloud.com>
2021-01-20 21:23:34 -0500

    Posting to Google Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -80,7 +81,24 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!!!');
+        //alert('You continue!!!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Angel',
+                address: {
+                    street: '73 Ice Berg Rd',
+                    zipCode: '1234',
+                    country:  'Canada'
+                },
+                email: 'test@email.com'
+            },
+            deliveryMethod: 'fastes'
+        };
+        axios.post('/orders.json', order)
+        .then(res => {console.log(res)})
+        .catch(err => {console.log(err)});
     }
 
     render() {
